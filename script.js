36464767460dc324d8f7c49b220bf2be9e6b8e26
Michał Bociek <mic.bociek@gmail.com>
2020-09-17 22:17:09 +0200

    js enging finished

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const currencyOne = document.querySelector('#currency-one');
-const amountOne = document.querySelector('.amount-one');
+const ammountOne = document.querySelector('.ammount-one');
 const currencyTwo = document.querySelector('#currency-two');
-const amountTwo = document.querySelector('.amount-one');
+const ammountTwo = document.querySelector('.ammount-two');
 const rateInfo = document.querySelector('.rate-info');
 const swapBtn = document.querySelector('.swap');
 
 //  Rates API https://ratesapi.io
-const url = 'https://api.ratesapi.io/api/latest';
 
-const httpGet = (theUrl) => {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+const calculate = () => {
+    rates = fetch(`https://api.ratesapi.io/api/latest?base=${currencyOne.value}&symbols=${currencyTwo.value}`)
+    .then(res => res.json())
+    .then(data => {
+        const currency1 = currencyOne.value;
+        const currency2 = currencyTwo.value;
+
+        const rate =data.rates[currency2];
+        rateInfo.textContent = `1 ${currency1} = ${rate.toFixed(4)} ${currency2}`;
+        console.log(rate);
+
+        ammountTwo.value = (ammountOne.value * rate).toFixed(2);
+    });
 }
 
+const swap = () =>{
+    [currencyOne.value, currencyTwo.value]  =[currencyTwo.value, currencyOne.value];
+    calculate();
+}
+
+
+currencyOne.addEventListener('change', calculate);
+currencyTwo.addEventListener('change', calculate);
+ammountOne.addEventListener('input', calculate);
+swapBtn.addEventListener('click', swap);
+calculate();
