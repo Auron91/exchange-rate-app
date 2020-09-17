b9ca8733963f7200aa62ea5707cc36976c954991
Michał Bociek <mic.bociek@gmail.com>
2020-09-17 21:11:27 +0200

    test

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const 
+const currencyOne = document.querySelector('#currency-one');
+const amountOne = document.querySelector('.amount-one');
+const currencyTwo = document.querySelector('#currency-two');
+const amountTwo = document.querySelector('.amount-one');
+const rateInfo = document.querySelector('.rate-info');
+const swapBtn = document.querySelector('.swap');
+
+//  Rates API https://ratesapi.io
+const url = 'https://api.ratesapi.io/api/latest';
+
+const httpGet = (theUrl) => {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
